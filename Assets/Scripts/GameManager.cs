790d60da2a8c5df64e3fd9cb4aa5a437e8ec97d8
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2022-06-11 18:36:46 +0300

    import hdrp, photon + set up basic project settings

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Game version")]
+    public string str_GameVersion;
+    [SerializeField] private TMP_Text txt_GameVersion;
+
+    [Header("Framerate")]
+    [SerializeField] private TMP_Text txt_fpsValue;
+
+    //private variables
+    private float timer;
+    private float deltaTime;
+
+    private void Awake()
+    {
+        txt_GameVersion.text = str_GameVersion;
+
+        Screen.SetResolution(1920, 1080, FullScreenMode.ExclusiveFullScreen);
+        QualitySettings.vSyncCount = 0;
+    }
+
+    private void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float msec = Mathf.FloorToInt(deltaTime * 1000.0f);
+        float fps = Mathf.FloorToInt(1.0f / deltaTime);
+
+        timer += Time.unscaledDeltaTime;
+        if (timer > 0.1f)
+        {
+            txt_fpsValue.text = fps + " (" + msec + ")";
+            timer = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            Screenshot();
+        }
+    }
+
+    //press F12 for screenshot
+    private void Screenshot()
+    {
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\LightsOff\Screenshots";
+
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        var screenshotName = "Screenshot_" + System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + ".png";
+        ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(folderPath, screenshotName), 4);
+    }
+}
